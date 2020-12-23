b2cf9b04d7b380908457f286e69406b860c5a4c1
Natraj Solai <linuxraja@gmail.com>
2020-12-23 03:03:42 +0000

    updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World My First Build!" );
+        System.out.println( "cloudwwh.com My First Build!" );
     }
 }
